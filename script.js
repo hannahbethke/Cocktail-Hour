b44a867e586e61095eae13437f3b3135f0e3daaf
hannahbethke <hannahnichole@gmail.com>
2022-06-08 23:08:07 -0600

    more changes to JS pseudo code

--- script.js
+++ script.js
@@ -8,7 +8,10 @@
 
 
 // LOOPS / IF, ELSE STATEMENTS
-
+    // searchByIngredient: 
+        // for every cocktail name that is returned by search (innerText at specific object or array location), 
+            // create and display li with cocktail name innerText and a button id = "recipe"
+        
 
 // FUNCTIONS
 
@@ -18,10 +21,13 @@
     // when button id = "searchByIngredient" is clicked, open or show dialog box id = "drinkList"
         // background (main page) changes to become inaccessible
         // "drinkList" dialog box is displayed with <li>name of cocktail and button id = "recipe"</li>
-        // when button id = "recipe" is clicked, dialog box id = "drinkList" is closed and dialog box id = "recipeCard" opens
      
     // when search by drink name button is clicked, open dialog box id = "recipeCard"
-    // when search by name button is clicked, open dialog box id = recipe card
+        // background (main page) changes to become inaccessible
+
+    // when button id = "recipe" is clicked, if dialog box id = "drinkList" is open, set to closed,
+        // else, dialog box id = "recipeCard" opens
+   
 
 
 
